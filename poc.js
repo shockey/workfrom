58f921552efd6b0a04da6954006034e96663ae98
Kyle Shockey <kyleshockey1@gmail.com>
2016-06-18 00:54:38 -0700

    Delete poc.js

--- poc.js
+++
@@ -1,9 +0,0 @@
-require('babel-core/register');
-
-var Workfrom = require('./lib');
-
-var wf = Workfrom({
-  appid: 'abcdef1234567890'
-});
-
-wf.places.near({ lat: '37.783575', long: '-122.409048', radius: 2 }).then(results => { console.log(results) });
